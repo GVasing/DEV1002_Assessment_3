14dffc557477eb4dfa3aeaf06eb4875e0dc565df
George Vasiliadis <gvasiliadis72@gmail.com>
2025-08-18 13:43:19 +1000

    'create_app()' function to create flask app added to main.py

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ import os
 # Installed imports
 from flask import Flask
 from dotenv import load_dotenv
+
+# Created Module Imports
+from init import db
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+
+    # Initialise SQL Database
+    db.init_app(app)
+
+    return app
