b513da23df8b55cb6f2fd4eb179a751a25117eac
George Vasiliadis <gvasiliadis72@gmail.com>
2025-08-19 12:39:58 +1000

    Initial Relation Model Diagram removed.

---
+++ models/plane.py
@@ -0,0 +1,14 @@
+# Explain why this is imported
+from init import db
+
+# Explain the purpose of this class
+class Plane(db.Model):
+    __tablename__ = "planes"
+
+    # Attributes of 'Plane' entity
+    id = db.Column(db.Integer, primary_key=True)
+    manufacturer = db.Column(db.String, nullable=False)
+    model = db.Column(db.String, nullable=False, unique=True)
+    range = db.Column(db.Float, nullable=False)
+    passenger_capacity = db.Column(db.Float, nullable=False)
+    fuel_capacity = db.Column(db.Float, nullable=False)
